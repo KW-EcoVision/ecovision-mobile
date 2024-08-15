61b22a15c2944ccd61eb5b59b1087a6af8a8d935
kymin <park48664866@gmail.com>
2024-08-16 00:03:43 +0900

    feat : 앱 로고 및 아이콘 적용

--- lib/view/page/SignupPage.dart
+++ lib/view/page/SignupPage.dart
@@ -28,104 +28,108 @@ class _SignupPageState extends State<SignupPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const Center(
-              child: Text(
-                'Sign Up',
-                style: TextStyle(fontSize: 40),
-              ),
-            ),
-            Column(
-              children: [
-                EcoTextField(
-                  prefixIcon: const Icon(Icons.badge_outlined),
-                  labelText: 'Name',
-                  width: MediaQuery.of(context).size.width - 32,
-                  height: MediaQuery.of(context).size.height / 10,
-                  enabledBorderColor: borderColorByValid(validator.isValidName),
-                  focusedBorderColor: borderColorByValid(validator.isValidName),
-                  radius: 10,
-                  isPassword: false,
-                  onChanged: (value) {
-                    userData.name = value;
-                    validator.isValidName =
-                        validator.validateName(userData.name!);
-                    setState(() {}); // UI 업데이트
-                  },
-                ),
-                EcoTextField(
-                  prefixIcon: const Icon(Icons.person_outline_sharp),
-                  labelText: 'ID',
-                  width: MediaQuery.of(context).size.width - 32,
-                  height: MediaQuery.of(context).size.height / 10,
-                  enabledBorderColor:
-                      borderColorByValid(validator.isValidUserId),
-                  focusedBorderColor:
-                      borderColorByValid(validator.isValidUserId),
-                  radius: 10,
-                  isPassword: false,
-                  onChanged: (value) {
-                    userData.userId = value;
-                    validator.isValidUserId =
-                        validator.validateUserId(userData.userId!);
-                    setState(() {}); // UI 업데이트
-                  },
-                ),
-                EcoTextField(
-                  prefixIcon: const Icon(Icons.lock_outline_sharp),
-                  labelText: 'Password',
-                  width: MediaQuery.of(context).size.width - 32,
-                  height: MediaQuery.of(context).size.height / 10,
-                  enabledBorderColor:
-                      borderColorByValid(validator.isValidPassword),
-                  focusedBorderColor:
-                      borderColorByValid(validator.isValidPassword),
-                  radius: 10,
-                  isPassword: true,
-                  onChanged: (value) {
-                    userData.password = value;
-                    validator.isValidPassword =
-                        validator.validatePassword(userData.password!);
-                    setState(() {});
-                  },
-                ),
-                EcoTextField(
-                  prefixIcon: const Icon(Icons.lock_outline_sharp),
-                  labelText: 'Confirm Password',
-                  width: MediaQuery.of(context).size.width - 32,
-                  height: MediaQuery.of(context).size.height / 10,
-                  enabledBorderColor: borderColorByValid(
-                      confirmPassword!.isEmpty
-                          ? null
-                          : (userData.password == confirmPassword)),
-                  focusedBorderColor: borderColorByValid(
-                      confirmPassword!.isEmpty
-                          ? null
-                          : (userData.password == confirmPassword)),
-                  radius: 10,
-                  isPassword: true,
-                  onChanged: (value) {
-                    confirmPassword = value;
-                    setState(() {}); // UI 업데이트
-                  },
-                ),
-              ],
-            ),
-            Center(
-              child: TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text(
-                  'OK',
-                  style: TextStyle(fontSize: 15),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Center(
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(fontSize: 40),
                 ),
               ),
-            ),
-          ],
+              Column(
+                children: [
+                  EcoTextField(
+                    prefixIcon: const Icon(Icons.badge_outlined),
+                    labelText: 'Name',
+                    width: MediaQuery.of(context).size.width - 32,
+                    height: MediaQuery.of(context).size.height / 10,
+                    enabledBorderColor:
+                        borderColorByValid(validator.isValidName),
+                    focusedBorderColor:
+                        borderColorByValid(validator.isValidName),
+                    radius: 10,
+                    isPassword: false,
+                    onChanged: (value) {
+                      userData.name = value;
+                      validator.isValidName =
+                          validator.validateName(userData.name!);
+                      setState(() {}); // UI 업데이트
+                    },
+                  ),
+                  EcoTextField(
+                    prefixIcon: const Icon(Icons.person_outline_sharp),
+                    labelText: 'ID',
+                    width: MediaQuery.of(context).size.width - 32,
+                    height: MediaQuery.of(context).size.height / 10,
+                    enabledBorderColor:
+                        borderColorByValid(validator.isValidUserId),
+                    focusedBorderColor:
+                        borderColorByValid(validator.isValidUserId),
+                    radius: 10,
+                    isPassword: false,
+                    onChanged: (value) {
+                      userData.userId = value;
+                      validator.isValidUserId =
+                          validator.validateUserId(userData.userId!);
+                      setState(() {}); // UI 업데이트
+                    },
+                  ),
+                  EcoTextField(
+                    prefixIcon: const Icon(Icons.lock_outline_sharp),
+                    labelText: 'Password',
+                    width: MediaQuery.of(context).size.width - 32,
+                    height: MediaQuery.of(context).size.height / 10,
+                    enabledBorderColor:
+                        borderColorByValid(validator.isValidPassword),
+                    focusedBorderColor:
+                        borderColorByValid(validator.isValidPassword),
+                    radius: 10,
+                    isPassword: true,
+                    onChanged: (value) {
+                      userData.password = value;
+                      validator.isValidPassword =
+                          validator.validatePassword(userData.password!);
+                      setState(() {});
+                    },
+                  ),
+                  EcoTextField(
+                    prefixIcon: const Icon(Icons.lock_outline_sharp),
+                    labelText: 'Confirm Password',
+                    width: MediaQuery.of(context).size.width - 32,
+                    height: MediaQuery.of(context).size.height / 10,
+                    enabledBorderColor: borderColorByValid(
+                        confirmPassword!.isEmpty
+                            ? null
+                            : (userData.password == confirmPassword)),
+                    focusedBorderColor: borderColorByValid(
+                        confirmPassword!.isEmpty
+                            ? null
+                            : (userData.password == confirmPassword)),
+                    radius: 10,
+                    isPassword: true,
+                    onChanged: (value) {
+                      confirmPassword = value;
+                      setState(() {}); // UI 업데이트
+                    },
+                  ),
+                ],
+              ),
+              Center(
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text(
+                    'OK',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
