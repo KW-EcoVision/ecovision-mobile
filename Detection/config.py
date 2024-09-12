a5305b3ab3bec833a10b8bfe3c50bd62b519cef8
sien <sptw88@gmail.com>
2024-09-12 20:13:27 +0900

    DEL

--- Detection/config.py
+++
@@ -1,7 +0,0 @@
-
-EPSILON = 1E-6
-IMAGE_SIZE = (224, 224)
-
-S = 7       # Divide each image into a SxS grid
-B = 2       # Number of bounding boxes to predict
-C = 20      # Number of classes in the dataset
