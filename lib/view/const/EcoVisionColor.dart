d1539131022e035dc1b3b6db044149550f2ea8c2
kymin <park48664866@gmail.com>
2024-10-31 08:57:55 +0900

    feat: 기본 색상 변경

--- lib/view/const/EcoVisionColor.dart
+++ lib/view/const/EcoVisionColor.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class EcoVisionColor {
   // static Color white = const Color(0xFFFFFFFF);
-  static Color background = const Color(0xFFFDFAEC);
+  // static Color background = const Color(0xFFFDFAEC);
+  static Color background = const Color(0xFFFFFFFF);
   static Color pastelGreen = const Color(0xFFC4E6C3);
   static Color neonGreen = const Color(0xFF57E55A);
   static Color mainGreen = const Color(0xFF2D830E);
